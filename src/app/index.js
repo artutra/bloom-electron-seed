9adabc3b95b8bff70fcc1f6da68dbd13ea4aaccd
Artur <artur_ldm@hotmail.com>
2015-11-16 18:37:11 -0300

    Adiciona basico do database no index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,21 @@
 (function () {
+	var path = require('path');
+	var Datastore = require('nedb');
+    var db = new Datastore({ filename: path.join(__dirname, '/database.db'), autoload: true });
+
+	db.findOne({ _id: 1 }, function (err, doc) {
+	  doc = doc || { _id: 1, counter: 0 };
+
+	  console.log('This example was executed ' + doc.counter + ' times. Last access time was ' + doc.lastSeetAt);
+
+	  doc.lastSeetAt = new Date();
+	  doc.counter++;
+
+	  db.update({ _id: 1 }, doc, { upsert: true }, function (err, num) {
+	    console.log('Updated ' + num + ' records');
+	  });
+	});
+
 	angular.module('GR-APP-TITLE', ['templates']);
 	
 })();
